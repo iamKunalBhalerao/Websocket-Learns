8fa34f97e5ab4970a1ca88d20ee7b990cb6f491f
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-12-27 10:44:31 +0530

    created simple chat room with join room and chat logic

--- connect-basic-server/index.ts
+++ connect-basic-server/index.ts
@@ -1,11 +1,96 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
+const PORT = 8080;
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: PORT });
+
+const clients = new Map();
+
+function broadcast(message: object, excludeClient: WebSocket | null = null) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN && client !== excludeClient) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
 
 wss.on("connection", (ws) => {
-  ws.send("Welcome to Websockets.");
+  console.log("New Client Connected!");
+
+  ws.send(
+    JSON.stringify({
+      type: "system",
+      message: "Welcome to Chat! Please send your username to join.",
+    })
+  );
 
   ws.on("message", (data) => {
-    ws.send(`You: ${data}`);
+    try {
+      const message = JSON.parse(data as unknown as string);
+
+      // Join the chat room
+      if (message.type === "join") {
+        clients.set(ws, message.username);
+
+        broadcast(
+          {
+            type: "system",
+            message: `${message.username} Joined the chat`,
+          },
+          ws
+        );
+
+        ws.send(
+          JSON.stringify({
+            type: "system",
+            message: `${message.username} Joined the chat`,
+          })
+        );
+      } else if (message.type === "chat") {
+        const username = clients.get(ws);
+
+        if (username) {
+          broadcast({
+            type: "chat",
+            username: username,
+            text: message.text,
+            timeStamp: new Date().toISOString(),
+          });
+        } else {
+          ws.send(
+            JSON.stringify({
+              type: "error",
+              message: "Please join with the username first",
+            })
+          );
+        }
+      }
+    } catch (error: any) {
+      console.log(`Error processing message: ${error}`);
+
+      ws.send(
+        JSON.stringify({
+          type: "error",
+          message: "Invalid Message format",
+        })
+      );
+    }
+  });
+
+  ws.on("close", () => {
+    const username = clients.get(ws);
+
+    if (username) {
+      broadcast({
+        type: "system",
+        message: `${username} left the chat.`,
+      });
+    }
+    clients.delete(ws);
+  });
+
+  ws.on("error", (error) => {
+    console.log(`Websocket Error: ${error}`);
   });
 });
+
+console.log(`WebSocket Server is running on ws://localhost:${PORT}`)
